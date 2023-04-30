ff9d1b815814d79ec59e66b0e9416da9d8e23ed6
zxcv2008a <zxcv2008a@gmail.com>
2023-04-30 15:06:12 +1000

    Merge with main

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,16 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://user:password@localhost/saltandpaper"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/saltandpaper"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Import models
-from models import User, Recipe, Step, Rating
+from src.backend.users.models.models import User, Recipe, Step, Rating
 
 if __name__ == "__main__":
     app.run()
