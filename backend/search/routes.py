46bf73ea4ec54b06550d6235423a169426f19b90
pranaypremdas <pranaypremdas@yahoo.com.au>
2023-06-03 01:05:07 +1000

    Dummy commit

--- backend/search/routes.py
+++ backend/search/routes.py
@@ -1,39 +1,31 @@
-from flask import Blueprint, jsonify, request
-from datetime import datetime, timedelta
-from sqlalchemy import exc
-from backend.models.models import Recipe
-from backend.models import db
+from flask import Blueprint, jsonify
+from backend.tasks.async_task import async_health
 
-from celery.result import AsyncResult
-import time
-from sqlalchemy import or_
 search_api = Blueprint('search_api', __name__, url_prefix='/api/v1/search')
 
-@search_api.route('/health') 
+from celery import Celery
+
+celery = Celery(
+    'backend',
+    broker='redis://redis:6379',
+    backend='redis://redis:6379'
+)
+
+# Import your tasks here
+from backend.tasks.async_task import async_health
+
+@search_api.route('/health')
 def health():
-    """Return a status of 'ok' if the server is running and listening to request"""
-    return jsonify({"status": "ok"})
+    try:
+        task = async_health.delay()
+        result = task.get()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": "Ticket not found"}), 801
 
-@search_api.route('/searchcategory/<string:keywords>') 
-def searchCategory(keywords):
-    docs = Recipe.query.all()
-    doc = [doc.to_dict() for doc in docs]
-    task = searchCatOrDes.delay(doc, keywords, True)
-    while True:
-        if task.ready():
-            task_result = task.result 
-            return task_result, 200
-        else:
-            time.sleep(1)
+@search_api.route('/searchcategory/<string:keywords>')
+def searchKeyword(keywords):
+    pass
 
-@search_api.route('/searchdescription/<string:keywords>') 
-def searchDescription(keywords):
-    docs = Recipe.query.all()
-    doc = [doc.to_dict() for doc in docs]
-    task = searchCatOrDes.delay(doc, keywords, False)
-    while True:
-        if task.ready():
-            task_result = task.result
-            return task_result, 200
-        else:
-            time.sleep(1)
+if __name__ == '__main__':
+    celery.start()
--- backend/tasks/async_task.py
+++ backend/tasks/async_task.py
@@ -2,13 +2,12 @@ import os
 from flask import jsonify
 from celery import Celery
 
+
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
-celery.conf.task_default_queue = os.environ.get(
-    "CELERY_DEFAULT_QUEUE", "async_exec")
-
+celery.conf.task_default_queue = os.environ.get("CELERY_DEFAULT_QUEUE", "async_exec")
 
 @celery.task(name="async_exec")
-def print_async_ticket():
+def async_health():
     return jsonify({"status": "ok"})
