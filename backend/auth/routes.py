dd17ef0dcb91df9d391a4f6a59a72b2032776b7e
pranaypremdas <pranaypremdas@yahoo.com.au>
2023-06-04 01:33:13 +1000

    Fixed register user bug

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -33,7 +33,7 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         new_user = User(name=form.name.data, username=form.username.data,
                         email=form.email.data,  password=hashed_password)
         db.session.add(new_user)
