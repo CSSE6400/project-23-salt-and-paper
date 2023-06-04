7fe314210ea0276890459dfe61f96e2bfb13a31f
Sourav-RC <110149206+Sourav-RC@users.noreply.github.com>
2023-06-04 08:00:58 +0530

    duplicate email allowed, css for login, signup

--- backend/models/models.py
+++ backend/models/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String, nullable=False)
     cooking_preferences = db.Column(db.String(100))
 
